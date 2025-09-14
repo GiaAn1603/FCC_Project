67022dd7dc5a94c42a433ed7c449aaa1d731978e
GiaAn1603 <anphamproductions@gmail.com>
2025-09-14 15:19:24 +0700

    Fix code formatting

--- JavaScript Algorithms and Data Structures/RPG Creature Search App/script.js
+++ JavaScript Algorithms and Data Structures/RPG Creature Search App/script.js
@@ -54,7 +54,7 @@ async function showCreature() {
         types.appendChild(document.createTextNode(", "));
       }
     });
-  } catch(error) {
+  } catch (error) {
     console.log(error);
   }
 }
